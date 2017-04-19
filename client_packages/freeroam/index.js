2b8054b0f38c2c953787b03bde82af18b6f7ea20
Nikita Batrak <niks.dn.ua@gmail.com>
2017-04-19 21:09:40 +0300

    - Minor fixes.

--- client_packages/freeroam/index.js
+++ client_packages/freeroam/index.js
@@ -11,24 +11,22 @@ let weaponInit   = require('freeroam/menu_initialization/weapon.js');
 let playersInit  = require('freeroam/menu_initialization/players.js')
 // Init events.
 require('freeroam/events.js')();
-// CEF inited?
-let CEF_inited = false;
 
 // Creating browser.
 mp.events.add('guiStarted', () => {
-    // Creating CEF browser.
-    menu = mp.browsers.new('package://freeroam/index.html');
-    // Init menus, when browser ready.
-    mp.events.add('browserDomReady', (browser) => {
-        if (browser == menu && !CEF_inited) {
-            vehiclesInit(menu, vehicles);
-            skinsinit(menu, skins);
-            weaponInit(menu, weapon);
-            playersInit(menu);
+    if (!menu) {
+        // Creating CEF browser.
+        menu = mp.browsers.new('package://freeroam/index.html');
+        // Init menus, when browser ready.
+        mp.events.add('browserDomReady', (browser) => {
+            if (browser == menu) {
+                vehiclesInit(menu, vehicles);
+                skinsinit(menu, skins);
+                weaponInit(menu, weapon);
+                playersInit(menu);
             
-            mp.gui.execute(`insertMessageToChat('<div style="background-color: rgba(0, 0, 0, 0.75); font-size: 1.0vw; padding: 6px; color: #ff0000; font-weight: 600;">Press F1 for open freeroam menu.</div>', 'true');`);
-            
-            CEF_inited = true;
-        }
-    });
+                mp.gui.execute(`insertMessageToChat('<div style="background-color: rgba(0, 0, 0, 0.75); font-size: 1.0vw; padding: 6px; color: #ff0000; font-weight: 600;">Press F1 for open freeroam menu.</div>', 'true');`);
+            }
+        });
+    }
 });
